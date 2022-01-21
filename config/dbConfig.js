bdaf6342c139a4ed7a65424f6a75f6367a9c45f0
adlinahmed <59899886+adlinahmed@users.noreply.github.com>
2022-01-20 20:56:54 -0600

    changed config stuff

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -1,7 +1,7 @@
 module.exports = {
-HOST: 'stocks-database-1.cr0hcaui9ivp.us-east-2.rds.amazonaws.com',
-USER: 'postgres_adlin', 
-PASSWORD: 'adlinpostgres', 
-DB: 'stocks_db', 
+HOST: 'ec2-3-225-41-234.compute-1.amazonaws.com',
+USER: 'aapddjucmgrwla', 
+PASSWORD: 'c19c86a7be0226b293cd85cc2dabcd95a7c9765cea293ce29e0673ddbf584ddc', 
+DB: 'd6b72i0t7l9nha', 
 dialect: 'postgres',
 }

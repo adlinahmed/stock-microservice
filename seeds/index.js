7013798aa03cb25eb9738bf52c1902b4cf7bb71a
adlinahmed <59899886+adlinahmed@users.noreply.github.com>
2022-01-26 11:47:10 -0600

    created db

---
+++ seeds/index.js
@@ -0,0 +1,42 @@
+const db = require('../models/index')
+
+const Stock = db.Stocks
+const fs = require('fs');
+
+module.exports = function () {
+    fs.readFile ('./stocks.csv', 'utf-8', function(error, data){
+        if (error) {
+            throw error 
+        }
+        // console.table (data);
+        let arr = data.split (',');
+        arr.splice (0,11);
+        console.log (arr);
+    
+        let stocks = [];
+        let stock = {};
+    
+        for (let i = 0; i < arr.length; i+=11) {
+            stock = {
+                symbol: arr[i].replace('\r\n', ''),
+                name: arr[i+1],
+                lastSale: arr[i+2],
+                netChange: arr[i+3],
+                changePercent: arr[i+4],
+                marketCap: arr[i+5],
+                country: arr[i+6],
+                ipoYear: arr[i+7],
+                volume: arr[i+8],
+                sector: arr[i+9],
+                industry: arr[i+10]
+            }
+            stocks.push(stock);
+        } 
+        console.log(stocks)
+    
+        stocks.forEach(async(stock) => {
+            await Stock.create (stock)
+        })
+    })
+}
+

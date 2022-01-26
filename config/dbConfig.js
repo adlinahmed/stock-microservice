2d0e27f251e6ac08b7a7ac487de88af05b013a9a
adlinahmed <59899886+adlinahmed@users.noreply.github.com>
2022-01-26 10:32:40 -0600

    added csv

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -1,7 +1,7 @@
 module.exports = {
-HOST: 'ec2-3-225-41-234.compute-1.amazonaws.com',
-USER: 'aapddjucmgrwla', 
-PASSWORD: 'c19c86a7be0226b293cd85cc2dabcd95a7c9765cea293ce29e0673ddbf584ddc', 
-DB: 'd6b72i0t7l9nha', 
-dialect: 'postgres',
-}
+    HOST: 'ec2-3-225-41-234.compute-1.amazonaws.com',
+    USER: 'aapddjucmgrwla', 
+    PASSWORD: 'c19c86a7be0226b293cd85cc2dabcd95a7c9765cea293ce29e0673ddbf584ddc', 
+    DB: 'd6b72i0t7l9nha', 
+    dialect: 'postgres',
+    }
